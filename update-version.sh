1b123153e72967ca19781ecbab02b4e39035f7c9
atticusofsparta <atticusofsparta@protonmail.com>
2024-10-24 10:44:43 -0600

    fix(version): update version script to target dist

--- update-version.sh
+++ update-version.sh
@@ -6,8 +6,5 @@ nextRelease_version=$1
 # Update the version in src/version.ts
 sed -i.bak -e "s/export const version = '.*';/export const version = '${nextRelease_version}';/" src/version.ts && rm src/version.ts.bak
 
-# Update the version in lib/cjs/version.js
-sed -i.bak -e "s/exports.version = '.*';/exports.version = '${nextRelease_version}';/" lib/cjs/version.js && rm lib/cjs/version.js.bak
-
-# Update the version in lib/esm/version.js
-sed -i.bak -e "s/export const version = '.*';/export const version = '${nextRelease_version}';/" lib/esm/version.js && rm lib/esm/version.js.bak
+# Update the version in dist/version.d.ts
+sed -i.bak -e "s/export const version = '.*';/export const version = '${nextRelease_version}';/" dist/version.d.ts && rm dist/version.d.ts.bak
